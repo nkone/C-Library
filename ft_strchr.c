3e24ae5a2636e934eeaafeef2067ad9df07c17bb
nkone <phtruong@protonmail.com>
2019-02-25 14:49:51 -0800

    minor changes

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: phtruong <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 21:32:51 by phtruong          #+#    #+#             */
-/*   Updated: 2019/02/14 21:42:58 by phtruong         ###   ########.fr       */
+/*   Updated: 2019/02/25 14:49:27 by phtruong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,14 @@
 ** RETURN VALUES
 ** strchr() return a pointer to the located character, or NULL if c not found.
 */
+
+/*
+** PSEUDOCODE
+** While s is not same as character c, move s forward
+** If pointer reaches NULL return NULL
+** Return pointer s otherwise
+*/
+
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
